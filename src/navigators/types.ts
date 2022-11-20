bfaae4aa4e5e220dc57d78bfc6298be1e7bbc769
jmedders <medders.jeffrey@gmail.com>
2022-11-20 12:15:23 -0500

    add poi/station details from nav props, start charging functionality

--- src/navigators/types.ts
+++ src/navigators/types.ts
@@ -1,4 +1,17 @@
+import { StackNavigationProp } from "@react-navigation/stack";
+import type { ExtendedPOIDetails } from "../types/POIDetails";
+
 export type RootStackParamList = {
-    Dashboard: undefined;
-    StationDetails: undefined;
+  Dashboard: undefined;
+  StationDetails: StationDetailsRoute;
+};
+
+export type NavigationRootStackType = StackNavigationProp<RootStackParamList>;
+
+export type RouteParams = {
+  StationDetails: StationDetailsRoute;
+};
+
+type StationDetailsRoute = {
+  poi: ExtendedPOIDetails;
 };
